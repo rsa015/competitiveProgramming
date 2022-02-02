14590d3ce4411cb1ba93a9ea068a21b2063f8c7a
rsa <s.adjignon015@hotmail.com>
2022-02-02 23:33:15 +0000

    Missing Number Problem: Solved

--- cses/1083.py
+++ cses/1083.py
@@ -1,3 +1,12 @@
 
 def missingNum(number, seq):
-    pass 
+    sumNum = sum([digits for digits in range(1, number+1)])
+    seqSum = sum(seq)
+    missDig = sumNum - seqSum
+    print(missDig)
+
+
+if __name__ == "__main__":
+    number = int(input())
+    seq = list(map(int, input().split()))
+    missingNum(number, seq)
