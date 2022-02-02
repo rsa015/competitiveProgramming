fd15d933cc470f01607dcb1063105968a3d7a9f9
rsa <s.adjignon015@hotmail.com>
2022-02-02 09:15:17 +0000

    Fixed: esle to else

--- cses/1068.py
+++ cses/1068.py
@@ -5,7 +5,7 @@ def weirdAlgo(number):
     while (number > 1):
         if number%2 == 0:
             number = number // 2
-        esle: number = number*3 + 1
+        else: number = number*3 + 1
         res.append(number)
     print(*res) # * unpacks the list
 
