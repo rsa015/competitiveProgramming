874a4602dabf411b6cf530bda6e13d5bc5c6897c
rsa <s.adjignon015@hotmail.com>
2022-02-02 11:32:01 +0000

    Missing Number: Solved

--- cses/1083.py
+++ cses/1083.py
@@ -1,4 +1,13 @@
 
-def missingNum(number, sequence):
-    pass
+def missingNum(number, seq):
+    seqNumber = [digits for digits in range(1, number+1)]
+    seqSum = sum(seq)
+    sumNum = sum(seqNumber)
+    missDig = sumNum - seqSum
+    print(missDig)
 
+
+if __name__ == "__main__":
+    number = int(input())
+    seq = list(map(int, input().split()))
+    missingNum(number, seq)
