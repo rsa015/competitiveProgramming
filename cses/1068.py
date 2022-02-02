e75608c312dba75d900f42a644d05fd54dd4f829
rsa <s.adjignon015@hotmail.com>
2022-02-02 09:00:33 +0000

    Weird Algorithm solved: Can be improved

--- cses/1068.py
+++ cses/1068.py
@@ -3,8 +3,13 @@
 def weirdAlgo(number):
     res = [number]
     while (number > 1):
-        if number%2 ==0:
+        if number%2 == 0:
             number = number // 2
         esle: number = number*3 + 1
         res.append(number)
     print(*res) # * unpacks the list
+
+
+if __name__ == "__main__":
+    number = int(input())
+    weirdAlgo(number)
