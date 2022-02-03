08be0d65a0d2c67f8dc6ded59722e537cda8480a
rsa <s.adjignon015@hotmail.com>
2022-02-03 01:25:38 +0000

    Repetitions Problem: Solved

--- cses/1069.py
+++ cses/1069.py
@@ -1,3 +1,17 @@
 
 def strOccurence(seq):
-    pass
+    eltOne = seq[0]
+    found = 0
+    allFound = 0
+    for dnaElt in seq+" ":
+        if dnaElt != eltOne:
+            allFound = max(allFound, found)
+            found = 1
+            eltOne = dnaElt
+        else: found+=1
+    return allFound
+
+
+if __name__ == "__main__":
+    seq = str(input()).replace(" ", "").upper()
+    print(strOccurence(seq))
