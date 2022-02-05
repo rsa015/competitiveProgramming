cadedbbe871dd300d13eb7507767f8ed637c9d9f
rsa <s.adjignon015@hotmail.com>
2022-02-05 05:04:25 +0000

    Increasing Array Problem: Solved

--- cses/1094.py
+++ cses/1094.py
@@ -1,3 +1,16 @@
 
-def inArray(n, seq):
-    pass
+def inArray(n, array):
+    prev = seq[0]
+    res = 0
+    
+    for elt in array:
+        if elt < prev:
+            res += prev-elt
+        else: prev = elt
+    print(res)
+
+if __name__ == "__main__":
+    n = int(input())
+    seq = list(map(int, input().split()))
+    inArray(n, seq)
+    
