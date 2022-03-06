50ce8c3a47e86184eea238ae785353a2b3f7dbe5
rsa <s.adjignon015@hotmail.com>
2022-03-06 21:45:50 +0000

    initial commit

---
+++ cses/1070.py
@@ -0,0 +1,9 @@
+# Permutations Problem
+
+def permutate(arrayLength):
+    """
+    Returns an array of length equal to the given argument,
+    in which the substraction of two consecutive elements is greater that 1.
+    """
+
+
